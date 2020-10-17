9daa0c3d8b402d6567787c16841b0cf8b5feed5d
Hugh Rawlinson <hughr2005@gmail.com>
2020-10-16 22:09:59 -0400

    make it a synthesizer

--- lib/synth.ts
+++ lib/synth.ts
@@ -1,38 +1,62 @@
+type OscillatorType = "custom" | "sawtooth" | "sine" | "square" | "triangle";
+
 type SynthBaseArgs = {
   audioContext: AudioContext
 }
 
 type SynthVoiceArgs = SynthBaseArgs & {
   note: Note
+  params: typeof params
+}
+
+function midiToFrequency(midiNote: number): number {
+  return Math.pow(2, (midiNote - 69) / 12) * 440;
 }
 
 class SynthVoice {
   audioContext: AudioContext;
-  oscillator: OscillatorNode;
+  oscillator1: OscillatorNode;
+  oscillator2: OscillatorNode;
   gain: GainNode;
+  limitGain: GainNode;
   note: Note;
 
-  constructor({ audioContext, note }: SynthVoiceArgs) {
+  constructor({ audioContext, note, params }: SynthVoiceArgs) {
     this.audioContext = audioContext;
     this.note = note;
-    this.oscillator = audioContext.createOscillator();
-    this.oscillator.frequency.value = Math.pow(2, (note.note - 69) / 12) * 440;
+    this.oscillator1 = audioContext.createOscillator();
+    params.oscillator1type.addEventListener((oscillatorType) => {
+      this.oscillator1.type = oscillatorType;
+    })
+    this.oscillator1.frequency.value = midiToFrequency(note.note);
+    this.oscillator2 = audioContext.createOscillator();
+    params.oscillator2type.addEventListener((oscillatorType) => {
+      this.oscillator2.type = oscillatorType;
+    })
+    this.oscillator2.frequency.value = midiToFrequency(note.note - 12);
     this.gain = audioContext.createGain();
     this.gain.gain.value = 1;
-    this.oscillator.connect(this.gain);
-    this.gain.connect(audioContext.destination)
+    this.limitGain = audioContext.createGain();
+    this.limitGain.gain.value = 0.3;
+    this.oscillator1.connect(this.gain);
+    this.oscillator2.connect(this.gain);
+    this.gain.connect(this.limitGain);
+    this.limitGain.connect(this.audioContext.destination);
   }
 
   start() {
-    this.oscillator.start();
+    this.oscillator1.start();
+    this.oscillator2.start();
 
     if (this.note.duration) {
-      this.oscillator.stop(this.note.duration + this.audioContext.currentTime);
+      this.oscillator1.stop(this.note.duration + this.audioContext.currentTime);
+      this.oscillator2.stop(this.note.duration + this.audioContext.currentTime);
     }
   }
 
   stop() {
-    this.oscillator.stop();
+    this.oscillator1.stop();
+    this.oscillator2.stop();
   }
 }
 
@@ -46,10 +70,51 @@ export type Note = {
   duration?: number
 }
 
+type SynthParamConstructorArgs<T> = {
+  initialValue: T
+}
+
+export class SynthParam<T> {
+  value: T;
+  eventListeners: ((_: T) => void)[] = []
+
+  constructor({ initialValue }: SynthParamConstructorArgs<T>) {
+    this.value = initialValue;
+  }
+
+  addEventListener(eventListener: (_: T) => void) {
+    this.eventListeners.push()
+    eventListener(this.value);
+    return this.eventListeners.length - 1;
+  }
+
+  removeEventListener(index: number) {
+    this.eventListeners.splice(index, 1);
+  }
+
+  getValue() {
+    return this.value;
+  }
+
+  setValue(v: T) {
+    this.value = v;
+    this.eventListeners.forEach(eventListener => {
+      eventListener(this.value);
+    });
+  }
+}
+
+const params = {
+  'oscillator1type': new SynthParam<OscillatorType>({ initialValue: "sawtooth" }),
+  'oscillator2type': new SynthParam<OscillatorType>({ initialValue: "sawtooth" })
+} as const;
+
+export type Params = typeof params;
+
 export class Synth {
   voicesInFlight: SynthVoice[] = [];
   audioContext: AudioContext;
-
+  params: typeof params = params;
   constructor({ audioContext }: SynthArgs) {
     this.audioContext = audioContext || new AudioContext();
     this.voicesInFlight = [];
@@ -61,10 +126,14 @@ export class Synth {
 
   playNote(note: Note) {
     console.log("Playing note: ", note)
-    const voice = new SynthVoice({ audioContext: this.audioContext, note });
+    const voice = new SynthVoice({ audioContext: this.audioContext, note, params: this.params });
     this.voicesInFlight.push(voice);
     voice.start();
     return voice;
+  }
+
+  getParams() {
+    return this.params;
   }
 
   panic() {
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,41 @@
 import Head from 'next/head'
-import { useSynth } from '../lib/useSynth';
+import { useSynth, isLoadedSynth } from '../lib/useSynth';
+import { SynthParam } from '../lib/synth';
+
+function OscillatorTypeSelector({ param }: { param: SynthParam<OscillatorType> }) {
+  return <select onChange={(event) => {
+    param.setValue(event.target.value as OscillatorType)
+  }}>
+    <option value="sawtooth">Sawtooth</option>
+    <option value="square">Square</option>
+    <option value="triangle">Triangle</option>
+    <option value="sin">Sine</option>
+  </select>
+}
 
 export default function Home() {
   const synth = useSynth();
 
-  if (synth.loading) {
+  if (!isLoadedSynth(synth)) {
     return <>
       <Head><title>Loading...</title></Head>
       <h1>Loading....</h1>
     </>
   }
-
-  return (
-    <>
-      <Head><title>The Synthesizer</title></Head>
-      <h1>The Synthesizer</h1>
-      <button onClick={() => synth.loading == false && synth.playNote({ note: 60, velocity: 127, duration: 1 })}>Play Note</button>
-    </>
-  )
+  else {
+    return (
+      <>
+        <Head><title>The Synthesizer</title></Head>
+        <h1>The Synthesizer</h1>
+        <button onClick={() => synth.playNote({ note: 50 + Math.floor(Math.random() * 24), velocity: 127, duration: 1 })}>Play Note</button>
+        <pre>
+          <code>
+            {JSON.stringify(synth.params, null, 2)}
+          </code>
+        </pre>
+        <OscillatorTypeSelector param={synth.params['oscillator1type']} />
+        <OscillatorTypeSelector param={synth.params['oscillator2type']} />
+      </>
+    )
+  }
 }
