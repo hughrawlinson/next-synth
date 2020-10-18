6b26e40c5cba2855bc2a0f41d0c865b6ef8c644f
Hugh Rawlinson <hughr2005@gmail.com>
2020-10-17 22:56:49 -0400

    Respect rehydration!

--- lib/useSynth.ts
+++ lib/useSynth.ts
@@ -1,4 +1,4 @@
-import React, { useRef } from 'react';
+import { useRef, useState, useEffect } from 'react';
 import { Synth, Note, Params } from './synth';
 
 function undefinedIfNotClient(provider: () => any) {
@@ -24,11 +24,26 @@ export function isLoadedSynth(synth: SynthHookResult): synth is LoadedSynth {
 
 type SynthHookResult = LoadingSynth | LoadedSynth;
 
+/*
+ * Since client side rehydration needs to render the same content as the pre-render
+ * does, we need to have a way to have useSynth know to return the loading state
+ * the first time, and then the actual response immediately after.
+ */
+function useHasMounted() {
+  const [hasMounted, setHasMounted] = useState(false);
+  useEffect(() => {
+    setHasMounted(true);
+  });
+
+  return hasMounted;
+}
+
 export function useSynth(): SynthHookResult {
+  const hasMounted = useHasMounted();
   const audioContext = useRef(undefinedIfNotClient(() => new AudioContext()));
   const synth = useRef<Synth>(undefinedIfNotClient(() => new Synth({ audioContext: audioContext.current })))
 
-  if (typeof audioContext.current == 'undefined' || typeof synth.current == 'undefined') {
+  if (typeof audioContext.current == 'undefined' || typeof synth.current == 'undefined' || !hasMounted) {
     return {
       loading: true
     }
